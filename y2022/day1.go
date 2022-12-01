b5fd7015f1aa4ce7926b205e7b46e311c87768f9
John Berthels <john.berthels@gmail.com>
2022-12-01 11:10:38 +0000

    Day 1 part 1

--- y2022/day1.go
+++ y2022/day1.go
@@ -3,6 +3,9 @@ package y2022
 import (
 	"fmt"
 	"io"
+
+	"github.com/jbert/aoc"
+	"github.com/jbert/aoc/fun"
 )
 
 type Day1 struct{ Year }
@@ -14,5 +17,17 @@ func NewDay1() *Day1 {
 
 func (d *Day1) Run(out io.Writer, lines []string) error {
 	fmt.Fprintf(out, "Running\n")
+	lgs := aoc.LineGroups(lines)
+	fmt.Printf("LGS:\n%v\n", lgs)
+
+	maxCalories := 0
+	for _, lg := range lgs {
+		ns := fun.Map(aoc.MustAtoi, lg)
+		calories := fun.Sum(ns)
+		if calories > maxCalories {
+			maxCalories = calories
+		}
+	}
+	fmt.Printf("Part1: %d\n", maxCalories)
 	return nil
 }
