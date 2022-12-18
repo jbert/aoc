09b3a8b08e885147cf1a5bd9e4f452d5e9b69756
John Berthels <john.berthels@gmail.com>
2022-12-18 08:51:22 +0000

    day 16 parse

--- y2022/day16.go
+++ y2022/day16.go
@@ -1,7 +1,13 @@
 package y2022
 
 import (
+	"fmt"
 	"io"
+	"strings"
+
+	"github.com/jbert/aoc/fun"
+	"github.com/jbert/aoc/graph"
+	"github.com/jbert/aoc/num"
 )
 
 type Day16 struct{ Year }
@@ -11,6 +17,38 @@ func NewDay16() *Day16 {
 	return &d
 }
 
+type Valve struct {
+	label    string
+	flowRate int
+	on       bool
+}
+
 func (d *Day16) Run(out io.Writer, lines []string) error {
+	valves := make(map[string]Valve)
+	var edges []graph.Edge[string]
+	for _, l := range lines {
+		v, lineEdges := parseLine(l)
+		valves[v.label] = v
+		edges = append(edges, lineEdges...)
+	}
+	g := graph.NewFromEdges(edges, true)
+	fmt.Printf("G: %v\n", g)
+	fmt.Printf("V: %v\n", valves)
 	return nil
+}
+
+func parseLine(l string) (Valve, []graph.Edge[string]) {
+	v := Valve{}
+	v.label = l[6:8]
+	l = l[23:]
+	i := strings.Index(l, ";")
+	v.flowRate = num.MustAtoi(l[:i])
+	l = l[i:]
+	l = l[18:]
+	i = strings.Index(l, " ")
+	l = l[i+1:]
+	fmt.Printf("[%s]\n", l)
+	dests := strings.Split(l, ", ")
+	edges := fun.Map(func(s string) graph.Edge[string] { return graph.Edge[string]{From: v.label, To: s} }, dests)
+	return v, edges
 }
