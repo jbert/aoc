e170a02ffef27359a8d695c84cd27c787f823ef9
John Berthels <john.berthels@gmail.com>
2023-10-23 14:54:00 +0100

    piece display

--- y2022/day17.go
+++ y2022/day17.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 
 	"github.com/jbert/aoc"
+	"github.com/jbert/fun"
 )
 
 type Day17 struct{ Year }
@@ -16,11 +17,54 @@ func NewDay17() *Day17 {
 }
 
 func (d *Day17) Run(out io.Writer, lines []string) error {
-	pieces := aoc.LineGroups(strings.Split(pieceStr, "\n"))
+	pieces := fun.Map(newPiece, aoc.LineGroups(strings.Split(pieceStr, "\n")))
 	for _, p := range pieces {
 		fmt.Printf("%s\n", p)
 	}
+
 	return nil
+}
+
+type piece struct {
+	x, y   int
+	bits   [][]bool
+	w, h   int
+	moving bool
+}
+
+func newPiece(lines []string) *piece {
+	p := piece{
+		bits: make([][]bool, len(lines)),
+		h:    len(lines),
+		w:    len(lines[0]),
+	}
+	for i, l := range lines {
+		p.bits[i] = make([]bool, p.w)
+		for j, r := range l {
+			p.bits[i][j] = r == '#'
+		}
+	}
+	return &p
+}
+
+func (p *piece) String() string {
+	b := &strings.Builder{}
+	fmt.Fprintf(b, "W %d H %d\n", p.w, p.h)
+	r := '#'
+	if p.moving {
+		r = '@'
+	}
+	for i := range p.bits {
+		for j := range p.bits[0] {
+			if p.bits[i][j] {
+				fmt.Fprintf(b, "%c", r)
+			} else {
+				fmt.Fprintf(b, " ")
+			}
+		}
+		fmt.Fprintf(b, "\n")
+	}
+	return b.String()
 }
 
 const pieceStr = `####
