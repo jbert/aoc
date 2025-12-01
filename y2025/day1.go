1bdade2573c7c601ef9a756c52dc8334ffe9fdd5
John Berthels <john.berthels@gmail.com>
2025-12-01 07:58:58 +0000

    day1: part1

--- y2025/day1.go
+++ y2025/day1.go
@@ -17,7 +17,23 @@ func NewDay1() *Day1 {
 }
 
 type Pos int
+
+func (p Pos) Perform(t Turn) Pos {
+	v := Pos(int(p) + int(t))
+	return v % 100
+}
+
 type Turn int
+
+func (t Turn) String() string {
+	it := int(t)
+	prefix := "R"
+	if it < 0 {
+		prefix = "L"
+		it *= -1
+	}
+	return fmt.Sprintf("%s%d", prefix, it)
+}
 
 func lineToTurn(l string) (Turn, error) {
 	if l == "" {
@@ -36,6 +52,9 @@ func lineToTurn(l string) (Turn, error) {
 	if err != nil {
 		return 0, fmt.Errorf("can't parse as int [%s]: %w", l[1:], err)
 	}
+	if step == 0 {
+		return 0, fmt.Errorf("zero step - check invariants")
+	}
 	return Turn(sign * step), nil
 }
 
@@ -47,7 +66,17 @@ func (d *Day1) Run(out io.Writer, lines []string) error {
 	if err != nil {
 		return fmt.Errorf("can't map lines to turns: %w", err)
 	}
-	fmt.Fprintf(out, "pos %d: %v\n", pos, turns)
+	// fmt.Fprintf(out, "pos %d: %v\n", pos, turns)
+	numZeros := 0
+	fmt.Fprintf(out, "The dial starts by pointing at %d.\n", pos)
+	for _, turn := range turns {
+		pos = pos.Perform(turn)
+		fmt.Fprintf(out, "The dial is rotated %s by to point at %d.\n", turn, pos)
+		if pos == 0 {
+			numZeros++
+		}
+	}
+	fmt.Fprintf(out, "password %d\n", numZeros)
 
 	return nil
 }
