777b906e9e45aba40f4224f2c2b7824dd2953bf1
John Berthels <john.berthels@gmail.com>
2025-12-01 10:24:54 +0000

    day2: part2

--- y2025/day1.go
+++ y2025/day1.go
@@ -21,6 +21,7 @@ type Pos int
 func (p Pos) Perform(t Turn) (Pos, int) {
 	it := int(t)
 	ip := int(p)
+
 	// Each full turn will go past zero
 	// and we can then discount it
 	zeroClicks := it / 100
@@ -30,11 +31,38 @@ func (p Pos) Perform(t Turn) (Pos, int) {
 	it = it % 100
 
 	v := ip + it
-	newPos := (v + 100) % 100
-	if v >= 100 || v <= 0 {
+	if v >= 100 || (ip > 0 && v <= 0) {
+		fmt.Printf("ip %d it %d v %d\n", ip, it, v)
 		zeroClicks++
 	}
+
+	newPos := (v + 100) % 100
 	return Pos(newPos), zeroClicks
+}
+
+func numToWord(n int) string {
+	if n == 1 {
+		return "once"
+	}
+	if n == 2 {
+		return "twice"
+	}
+	// thrice!
+	wordNums := map[int]string{
+		0: "zero",
+		3: "three",
+		4: "four",
+		5: "five",
+		6: "six",
+		7: "seven",
+		8: "eight",
+		9: "nine",
+	}
+	word, ok := wordNums[n]
+	if ok {
+		return word + " times"
+	}
+	return fmt.Sprintf("%d times", n)
 }
 
 // func (p Pos) Perform(t Turn) Pos {
@@ -92,14 +120,18 @@ func (d *Day1) Run(out io.Writer, lines []string) error {
 	for _, turn := range turns {
 		newPos, turnZeroClicks := pos.Perform(turn)
 		pos = newPos
-		fmt.Fprintf(out, "The dial is rotated %s by to point at %d.\n", turn, pos)
+		suffix := "."
+		if pos != 0 && turnZeroClicks > 0 {
+			suffix = "; during this rotation, it points at 0 " + numToWord(turnZeroClicks) + "."
+		}
+		fmt.Fprintf(out, "The dial is rotated %s by to point at %d%s\n", turn, pos, suffix)
 		if pos == 0 {
 			numZeros++
 		}
 		zeroClicks += turnZeroClicks
 	}
 	fmt.Fprintf(out, "Part1: password %d\n", numZeros)
-	fmt.Fprintf(out, "Part2: password %d\n", zeroClicks+numZeros)
+	fmt.Fprintf(out, "Part2: password %d\n", zeroClicks)
 
 	return nil
 }
