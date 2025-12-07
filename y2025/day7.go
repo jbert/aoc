09ced3fab19fbb1c5742fe9b3d222d66e8ba10b5
John Berthels <john.berthels@gmail.com>
2025-12-07 08:09:34 +0000

    day 7: part 2

--- y2025/day7.go
+++ y2025/day7.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 
 	"github.com/jbert/aoc/year"
+	"github.com/jbert/fun"
 	"github.com/jbert/set"
 )
 
@@ -17,24 +18,34 @@ func (d *Day7) Run(out io.Writer, lines []string) error {
 	sPos := strings.Index(lines[0], "S")
 	beams := []int{sPos}
 	splits := 0
+
+	bts := make([]int, len(lines[0]))
+	bts[sPos] = 1
+
 	for j := range lines {
 		if j == 0 {
 			continue
 		}
 		newBeams := set.New[int]()
+		nbts := make([]int, len(lines[0]))
 		for _, beam := range beams {
 			if lines[j][beam] == '^' {
 				newBeams.Insert(beam - 1)
 				newBeams.Insert(beam + 1)
+				nbts[beam-1] += bts[beam]
+				nbts[beam+1] += bts[beam]
 				splits += 1
 			} else {
 				newBeams.Insert(beam)
+				nbts[beam] += bts[beam]
 			}
 		}
 		beams = newBeams.ToList()
+		bts = nbts
 	}
 	fmt.Printf("Part 1: %d\n", splits)
-	// fmt.Printf("Part 2: %d\n", fun.Sum(joltages))
+
+	fmt.Printf("Part 2: %d\n", fun.Sum(bts))
 
 	return nil
 }
